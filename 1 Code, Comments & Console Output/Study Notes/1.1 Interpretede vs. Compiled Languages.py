59bf93a13f970496d45b52d9834a9d784307c8cf
CosmicLM <edsldev25@gmail.com>
2025-11-12 21:40:20 -0700

    Studied 1.1

---
+++ 1 Code, Comments & Console Output/Study Notes/1.1 Interpretede vs. Compiled Languages.py
@@ -0,0 +1,33 @@
+#machine language is binary code, so, either 0 or 1. so
+#in order for a computer to run any code, it will translate it somehow to 
+#binary data.
+
+#Binary Alphabet
+
+
+01000001 = "A"
+01100001 = "a"
+01000010 = "B"
+01100010 = "b"
+01000011 = "C"
+01100011 = "c"
+
+
+
+
+#Compiled languages are programming languages that can only be
+#used passing by a middleman (the compiler) to get to the computer
+#these languages are very fast because the instructions are precompiled to passed to the machine.
+
+#Some examples are Cobol, C, C++, Go, Rust
+
+
+#Interpreted languages are programming languages which instructions are precompiled for computers to read.
+#because of this, interpreted languages are usually slower, because it uses an interpreter as the middle man 
+
+#Interpreted Clanguages are: Python, Java, PHP 
+
+# sources: https://www.educative.io/blog/compiled-vs-interpreted-language
+#https://www.geeksforgeeks.org/compiler-design/difference-between-compiled-and-interpreted-language/
+#https://www.freecodecamp.org/news/compiled-versus-interpreted-languages/ 
+
